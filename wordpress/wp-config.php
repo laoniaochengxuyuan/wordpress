25eb7c0c5d5a9f653a4f38d55d3a09ebb60f5ad1
laoniao <772364149@qq.com>
2017-04-16 01:39:54 +0800

    测试用的案例

---
+++ wordpress/wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * WordPress基础配置文件。
+ *
+ * 这个文件被安装程序用于自动生成wp-config.php配置文件，
+ * 您可以不使用网站，您需要手动复制这个文件，
+ * 并重命名为“wp-config.php”，然后填入相关信息。
+ *
+ * 本文件包含以下配置选项：
+ *
+ * * MySQL设置
+ * * 密钥
+ * * 数据库表名前缀
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/zh-cn:%E7%BC%96%E8%BE%91_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
+/** WordPress数据库的名称 */
+define('DB_NAME', 'wordpres');
+
+/** MySQL数据库用户名 */
+define('DB_USER', 'root');
+
+/** MySQL数据库密码 */
+define('DB_PASSWORD', '123456');
+
+/** MySQL主机 */
+define('DB_HOST', 'localhost');
+
+/** 创建数据表时默认的文字编码 */
+define('DB_CHARSET', 'utf8mb4');
+
+/** 数据库整理类型。如不确定请勿更改 */
+define('DB_COLLATE', '');
+
+/**#@+
+ * 身份认证密钥与盐。
+ *
+ * 修改为任意独一无二的字串！
+ * 或者直接访问{@link https://api.wordpress.org/secret-key/1.1/salt/
+ * WordPress.org密钥生成服务}
+ * 任何修改都会导致所有cookies失效，所有用户将必须重新登录。
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'oEAS)m!7lxt<}I=^Alq}se-eQh|W498;h+_FvUS9 T@~.S[sjKM`AXepo1Q^);/?');
+define('SECURE_AUTH_KEY',  '=6xJ{YfFK:]U C>mp3RmEq.r;V72Zyn3/UA=z$rZ3Iv/bE<j,2}K:oeeP:V,ci;G');
+define('LOGGED_IN_KEY',    '^R3o+]gKe|>F6:#y#SP_$w+X~bo?7Zk?^qM$w&=ee5I+a#j%2Or[L,rP`kqUR3sJ');
+define('NONCE_KEY',        'G0LZSm|u[+hOqY?OAnW2`=h85<9H$?w.n;|f6|{b0Lz2t:& 8U$A99o)4Y#$hIK%');
+define('AUTH_SALT',        'WsFUA-*d+D.v1&J2S0&BTD|v;}JIiEM#6@n5NO;r|3ESYN>JH&uPKiJs|#Xc(td0');
+define('SECURE_AUTH_SALT', '0z>K=sTN=]jhX{*qJ,Tb-@wgql$W=a=>^VoMnSD|#e&b}4d?9ahKEpV@cLVKV5HP');
+define('LOGGED_IN_SALT',   '^LMRTh?$q9gKYu]xC?z:y-70R]@ezW*[)NfN#1vi$OP2PxlKy^M1kq:VH1W>^<.e');
+define('NONCE_SALT',       ':M1*{wz =+tt]_zN{[Zx2Xjez#$hwKE79LD7XEal@|C7vAtnP?b%j0at?U3GwY{u');
+
+/**#@-*/
+
+/**
+ * WordPress数据表前缀。
+ *
+ * 如果您有在同一数据库内安装多个WordPress的需求，请为每个WordPress设置
+ * 不同的数据表前缀。前缀名只能为数字、字母加下划线。
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * 开发者专用：WordPress调试模式。
+ *
+ * 将这个值改为true，WordPress将显示所有用于开发的提示。
+ * 强烈建议插件开发者在开发环境中启用WP_DEBUG。
+ *
+ * 要获取其他能用于调试的信息，请访问Codex。
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', true);
+
+/**
+ * zh_CN本地化设置：启用ICP备案号显示
+ *
+ * 可在设置→常规中修改。
+ * 如需禁用，请移除或注释掉本行。
+ */
+define('WP_ZH_CN_ICP_NUM', true);
+
+/* 好了！请不要再继续编辑。请保存本文件。使用愉快！ */
+
+/** WordPress目录的绝对路径。 */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** 设置WordPress变量和包含文件。 */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wordpress/wp-content/themes/test/header.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* The Header for my blog.
+*
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>我的旅游博客</title>
+</head>
+<body>
+	
+</body>
+<script type="text/javascript">
+	
+</script>
+</html>
